32ce370e1573ea67045d53005a12b32ad1f52422
ashwamegh <shashank7200@gmail.com>
2026-06-01 00:16:35 +0530

    chore(vscode): configure biome settings, update test status to passed, and remove obsolete error context file

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "scope-empty": [2, "never"],
+    "header-max-length": [2, "always", 150],
+    "body-max-line-length": [2, "always", 500],
   },
 };
